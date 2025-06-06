db893ed1369a73e50be086cee62cb9b277a8ba04
ragu <raghupathiragu776@gmail.com>
2025-06-06 21:47:11 +0530

    Fix Employee model field name and update API

--- employee_api/employee/models.py
+++ employee_api/employee/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Employee(models.Model):
     name= models.CharField(max_length=100)
     email = models.EmailField(unique = True)
-    posistion = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
--- employee_api/employee_api/settings.py
+++ employee_api/employee_api/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import pymysql
+pymysql.install_as_MySQLdb()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'employee_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rest',
+        'USER': 'root',
+        'PASSWORD': 'Ragu9659@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
