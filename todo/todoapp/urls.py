bac4dcef64ac63d56ab2e1194ee1fe39aa2181f4
ragu <raghupathiragu776@gmail.com>
2025-06-07 13:32:37 +0530

    Update URL configuration and other code changes

--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -1,3 +1,5 @@
+
+"""
 from django.urls import path
 from .views import (
     TodoItemListCreateView,
@@ -7,4 +9,16 @@ from .views import (
 urlpatterns = [
     path('todos/', TodoItemListCreateView.as_view(), name='todo-list-create'),
     path('todos/<int:pk>/', TodoItemRetrieveUpdateDestroyView.as_view(), name='todo-detail'),
+]
+"""
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import TodoItemViewSet
+
+router = DefaultRouter()
+router.register(r'employees', TodoItemViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,3 +1,5 @@
+
+"""
 from rest_framework import generics
 from .models import TodoItem
 from .serializers import TodoItemSerializer
@@ -7,5 +9,14 @@ class TodoItemListCreateView(generics.ListCreateAPIView):
     serializer_class = TodoItemSerializer
 
 class TodoItemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
+
+    """
+from rest_framework import viewsets
+from .models import TodoItem
+from .serializers import TodoItemSerializer
+
+class TodoItemViewSet(viewsets.ModelViewSet):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
